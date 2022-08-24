1e573b1f7748a9bbd4bb9b1112fd092836535b92
Lmesa0622 <110851440+Lmesa0622@users.noreply.github.com>
2022-08-24 13:49:14 -0500

    Add files via upload

---
+++ Reto seguimiento.py
@@ -0,0 +1,71 @@
+import datetime
+from datetime import date, time, datetime
+class Product: 
+    def __init__(self, id, name, price):
+        self.id= id
+        self.name= name
+        self.price= float
+        self.price_history= dict {date:price}
+
+class User:
+    def __int__(self, id, user_name):
+        self.id = id
+        self.user_name = user_name
+        self.balance = 0
+        self.order_list= []
+car = []   
+user_one = User(123,"Catalina Monsalve")
+print(f"Bienvenido {user_one.user_name}, su saldo actual es: {user_one.amount}")
+
+    def add_product_to_car(self, prod: Product):
+        p1 = Product(1,"Arroz", 1.000)
+        p2 = Product(2,"Papas", 3.000)
+        p3 = Product(3,"Carne", 10.500) 
+        p4 = Product(4,"Pollo", 9.000)
+        p5 = Product(5,"Salsa", 7.500)
+
+        car.append(p1)
+        car.append(p2)
+        car.append(p3)
+        car.append(p4)
+        car.append(p5)
+        i=0
+        while i<len(car):
+            print(car[i].id," Product: ",car[i].name, "- Price:", car[i].price,"$")
+            i+=1
+    add_product_to_car()
+
+    def consolidate_order(self, order_id: int):
+        a = a
+    
+    def add_to_balance(self, amount:float):
+        self.amount = 90.000
+        return self.amount
+    add_to_balance()
+    
+    def plot_order_history():
+
+class Order:
+    def __init__(self,id):
+        self.id= id
+        self.product_list= []
+        self.date= date
+        self.total= float
+        self.status= bool
+
+
+
+p1 = Product(1,"Arroz", 1.000)
+p2 = Product(2,"Papas", 3.000)
+p3 = Product(3,"Carne", 10.500) 
+p4 = Product(4,"Pollo", 9.000)
+p5 = Product(5,"Salsa", 7.500)|
+
+
+
+
+
+
+
+
+        
